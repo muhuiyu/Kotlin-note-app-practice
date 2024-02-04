f4ed940b5b818c5ab46f05082b0e413c24df14d5
muhuiyu <muhuiyu.grace@gmail.com>
2024-02-04 22:09:07 +0800

    Add AddNoteUseCase

--- app/src/main/java/com/example/mynote/di/AppModule.kt
+++ app/src/main/java/com/example/mynote/di/AppModule.kt
@@ -5,6 +5,7 @@ import androidx.room.Room
 import com.example.mynote.feature_note.data.data_source.NoteDatabase
 import com.example.mynote.feature_note.data.repository.NoteRepositoryImpl
 import com.example.mynote.feature_note.domain.repository.NoteRepository
+import com.example.mynote.feature_note.domain.use_case.AddNoteUseCase
 import com.example.mynote.feature_note.domain.use_case.DeleteNoteUseCase
 import com.example.mynote.feature_note.domain.use_case.GetNotesUseCase
 import com.example.mynote.feature_note.domain.use_case.NoteUseCases
@@ -38,7 +39,8 @@ object AppModule {
     fun provideNoteUseCases(repository: NoteRepository): NoteUseCases {
         return NoteUseCases(
             getNotes = GetNotesUseCase(repository),
-            deleteNote = DeleteNoteUseCase(repository)
+            deleteNote = DeleteNoteUseCase(repository),
+            addNote = AddNoteUseCase(repository)
         )
     }
 }
--- app/src/main/java/com/example/mynote/feature_note/domain/use_case/NoteUseCases.kt
+++ app/src/main/java/com/example/mynote/feature_note/domain/use_case/NoteUseCases.kt
@@ -2,5 +2,6 @@ package com.example.mynote.feature_note.domain.use_case
 
 data class NoteUseCases(
     val getNotes: GetNotesUseCase,
-    val deleteNote: DeleteNoteUseCase
+    val deleteNote: DeleteNoteUseCase,
+    var addNote: AddNoteUseCase
 )
